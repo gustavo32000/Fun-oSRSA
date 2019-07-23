ef088829c2aafaedae1adfae7b5a5e2ab6601d3d
gugu <gustavocorrea2032@gmail.com>
2019-07-23 12:39:19 -0300

    SRSA

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FunçãoSRSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           minhaData();
+        }
+
+        /// <summary>
+        /// Essa função exibe na tela do terminal a data e a hora do sistema
+        /// </summary>
+        static void minhaData(){
+            Console.WriteLine(DateTime.Now);//Mostra a data e a hora
+        }
+    }
+}
